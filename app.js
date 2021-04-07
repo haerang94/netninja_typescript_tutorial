1f16e513a7b449240ba402e710026c102cf885d5
haerangChoi <gofkdvjvl@naver.com>
2021-04-07 15:37:24 +0900

    feat: function type

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-//  Union Type & literal types & type alias
-function combine(input1, input2, resultConversion) {
-    var result; // 어떤 타입이든 들어감
-    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        // 둘 다 문자열이거나 숫자+문자열인 경우
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+//  function return types and void
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, "as-number");
-var combineStringAges = combine("30", "26", "as-number");
-var combineNames = combine("sara", "choi", "as-text");
-console.log(combineAges);
-console.log(combineStringAges);
-console.log(combineNames);
+function printResult(num) {
+    console.log("Result: " + num);
+    //return; // 이럴 경우 undefined로 결과 타입을 줄 수 있으나 void로 리턴하는게 좋음
+}
+printResult(add(2, 3));
+//let someValue:undefined;
+// let combineValuse;
+// let combineValues:Function;
+var combineValues;
+combineValues = add;
+// combineValues=5;//이럴 경우 에러남
+// combineValues=printResult;
+printResult(combineValues(8, 8));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (num) {
+    console.log(num);
+    return num; //무시된다. 리턴 void 타입은 리턴으로 뭘하든 무시하겠다는 뜻이다.
+});
