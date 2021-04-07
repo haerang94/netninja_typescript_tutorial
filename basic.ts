0fd591687fa88e77e0f63bfb6c6bc675f5c36344
haerangChoi <gofkdvjvl@naver.com>
2021-04-07 11:46:22 +0900

    feat: core type

---
+++ basic.ts
@@ -0,0 +1,12 @@
+function add(n1:number,n2:number,showResult:boolean){
+    if(showResult){
+        console.log(n1+n2);
+    }else return n1+n2;
+}
+let number;
+number="string"; //앞에 타입 지정을 안해서 가능
+number=24;
+const num1=5;
+const num2=2.8;
+const printResult=true;
+console.log(add(num1,num2,printResult));
